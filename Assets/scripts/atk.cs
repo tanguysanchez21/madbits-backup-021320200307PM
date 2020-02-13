2d8a27bc06c2547e0cc3fcf13f9c4ef3ead89c7d
tanguy21 <tanguysanchez21@gmail.com>
2020-02-13 12:46:39 +0100

    ennemy idle

--- Assets/scripts/atk.cs
+++ Assets/scripts/atk.cs
@@ -16,17 +16,13 @@ public class atk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(animator.GetBool("attack") == false && Input.touchCount > 0)
-        {
+        if (animator.GetBool("attack") == false && Input.touchCount > 0)
             animator.SetBool("attack", true);
-        }
         else
-        {
             animator.SetBool("attack", false);
-        }
     }
 
-    void Fire()
+    void FireSword()
     {
         GameObject projectile = Instantiate(sword, transform.position, Quaternion.identity);
         projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(5.0f, 0.0f);
